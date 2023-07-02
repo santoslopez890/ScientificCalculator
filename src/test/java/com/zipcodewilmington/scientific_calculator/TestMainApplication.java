cfe5db1e762f0b9f78ba8f165f93063cfec79715
Danielle <deegreene2110@gmail.com>
2023-07-02 17:44:14 -0400

    Done and Dusted

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -1,7 +1,41 @@
 package com.zipcodewilmington.scientific_calculator;
 
+
+import org.junit.Assert;
+import org.junit.Test;
+
 /**
  * Created by leon on 2/9/18.
  */
-public class TestMainApplication {
-}
+
+//public class TestMainApplication {
+
+    //test makes sure it is run as a test
+    //@Test
+    //public void testAddition() {
+
+        //call main class, make new class for test
+        //can name method after the result you are looking for
+
+        //Calculator calculator1 = new Calculator();
+
+        //int result = calculator1.add(5,10);
+
+       // Assert.assertEquals(15,result);
+
+
+        //java.lang.String string1 = mainApplication1(3 + 5);
+    //}
+    //public void testDivide() {
+
+        //testAddition();
+    //}
+
+//}
+
+
+
+
+
+
+
