9f261e56519b5c80caa5df43cb90069f2c8f8e1b
santos <santoslopez890@gmail.com>
2023-07-02 01:04:16 -0400

    all functions work need to add switch display modes and memory

--- src/main/java/com/zipcodewilmington/scientificcalculator/Scientific.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/Scientific.java
@@ -1,115 +1,121 @@
 package com.zipcodewilmington.scientificcalculator;
 import java.lang.Math;
+import java.util.Scanner;
 
 public class Scientific {
     public static void main(String[] args) {
         Console.println("Welcome to my calculator!");
-        String s = Console.getStringInput("Enter a string");
-        Integer i = Console.getIntegerInput("Enter an integer");
-        Double d = Console.getDoubleInput("Enter a double.");
+        //String s = Console.getStringInput("Enter a string");
+        //Integer i = Console.getIntegerInput("Enter an integer");
+       // Double d = Console.getDoubleInput("Enter a double.");
 
-        Console.println("The user input %s as a string", s);
-        Console.println("The user input %s as a integer", i);
-        Console.println("The user input %s as a d", d);
+      //  Console.println("The user input %s as a string", s);
+     //   Console.println("The user input %s as a integer", i);
+     //   Console.println("The user input %s as a d", d);
+        //testing funcions
+        boolean radTrueOrFalse = getInputRad();
+        double answer = inverseTan(50,radTrueOrFalse);
+        System.out.println(answer);
     }
 
+    static boolean getInputRad() {
 
-    public double sine(double userInput, boolean rad) {
+        return true;
+    }
+    public static double sine(double userInput, boolean rad) {
         //sin function user inputs number and checks if it's on radian mode or not
         double result;
         if (rad) {
             result = Math.toRadians(userInput);
-            return Math.sin(result);
+
         } else {
             result = Math.toDegrees(userInput);
-            return Math.sin(result);
+
         }
+        return Math.sin(result);
     }
 
-    public double cosine(double userInput, boolean rad) {
+    public static double cosine(double userInput, boolean rad) {
         double result;
         if (rad) {
             result = Math.toRadians(userInput);
-            return Math.cos(result);
+
         } else {
             result = Math.toDegrees(userInput);
-            return Math.cos(result);
-        }
 
+        }
+        return Math.cos(result);
     }
 
-    public double tan(double userInput, boolean rad) {
+    public static double tan(double userInput, boolean rad) {
         double result ;
         if (rad) {
             result = Math.toRadians(userInput);
-            return Math.tan(result);
+
         } else {
             result = Math.toDegrees(userInput);
-            return Math.tan(result);
-        }
 
+        }
+        return Math.tan(result);
     }
 
-    public double inverseSine(double userInput, boolean rad) {
+    public static double inverseSine(double userInput, boolean rad) {
         double result;
         if (rad) {
             result = Math.toRadians(userInput);
-            return Math.asin(result);
         } else {
             result = Math.toDegrees(userInput);
-            return Math.asin(result);
         }
+        return Math.asin(result);
     }
 
-    public double inverseCosine(double userInput, boolean rad) {
+    public static double inverseCosine(double userInput, boolean rad) {
         double result ;
         if (rad) {
             result = Math.toRadians(userInput);
-            return Math.acos(result);
+
         } else {
             result = Math.toDegrees(userInput);
-            return Math.acos(result);
-        }
 
+        }
+        return Math.acos(result);
     }
 
-    public double inverseTan(double userInput, boolean rad) {
+    public static double inverseTan(double userInput, boolean rad) {
         double result ;
         if (rad) {
             result = Math.toRadians(userInput);
-            return Math.atan(result);
         } else {
             result = Math.toDegrees(userInput);
-            return Math.atan(result);
         }
-
+        return Math.atan(result);
     }
 
-    public double log(double userInput) {
+    public static double log(double userInput) {
         double result;
         result = Math.log10(userInput);
         return result;
     }
 
-    public double inverseLog(double userInput) {
+    public static double inverseLog(double userInput) {
         double result;
         result = Math.exp(Math.log(userInput));
         return result;
     }
 
-    public double inverseNaturalLog(double userInput) {
+    public static double inverseNaturalLog(double userInput) {
         double result;
         result = Math.log(userInput);
         return Math.exp(result);
     }
 
-    public double ln(double userInput) {
+    public static double ln(double userInput) {
         double result;
         result = Math.log10(userInput);
         return result;
     }
 
-    public double factorial(int userInput) {
+    public static double factorial(int userInput) {
 
         int fact = userInput;
         for (int i = 1; i <= fact; i++) {
